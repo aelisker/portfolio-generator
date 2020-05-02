b1aa8cb6ae08ed451cde7b8e91bcf5d58c2be34e
Aaron Lisker <aelisker@gmail.com>
2020-05-01 19:54:27 -0700

    page generates

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-//no parentheses around the profileDataArr parameter
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const generatePage = (name, github) => {
+  return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
 
-  console.log('=============');
-
-  profileDataArr.forEach(profileItem => 
-    console.log(profileItem));
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+  `;
 };
+console.log(name, github);
+console.log('==============');
+console.log(generatePage(name, github));
 
-printProfileData(profileDataArgs);
+
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// //no parentheses around the profileDataArr parameter
+// const printProfileData = profileDataArr => {
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('=============');
+
+//   profileDataArr.forEach(profileItem => 
+//     console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
